70e1a28e4ce3df7f22a6128654e900443c5c14ad
zhaokaiyu124 <32445159+zhaokaiyu124@users.noreply.github.com>
2019-11-14 21:41:07 -0800

    Update leduc.py
    add oracle player

--- leduc.py
+++ leduc.py
@@ -9,7 +9,7 @@ class Poker:
       currentPlayer (str): Player1, Player2, Dealer (which agent is going to take an action)
       player1Hand (list): card1, card2
       player2Hand (list): card1, card2
-      player1Actions (list): check, call, ...
+      player1legalActions (list): check, call, ...
       communityCards (list): card1, card2, ...
       deck: current deck
       player1Bets (list): bet1, bet2 (these are additions to the pot)
@@ -30,7 +30,7 @@ class Poker:
         self.state['currentPlayer'] = 'Player1'
         self.state['player1Hand'] = [self.state['deck'].draw(1) for i in range(hand_size)]
         self.state['player2Hand'] = [self.state['deck'].draw(1) for i in range(hand_size)]
-        self.state['player1Actions'] = []
+        self.state['player1legalActions'] = []
         self.state['communityCards'] = self.state['deck'].draw(3)
         self.state['player1Bets'] = []
         self.state['player2Bets'] = []
@@ -46,7 +46,7 @@ class Poker:
         Card.print_pretty_cards(self.state['player1Hand'])
         print('player2Hand:')
         Card.print_pretty_cards(self.state['player2Hand'])
-        print('player1Actions:{}'.format(self.state['player1Actions']))
+        print('player1legalActions:{}'.format(self.state['player1legalActions']))
         print('communityCards')
         Card.print_pretty_cards(self.state['communityCards'])
         print('player1Bets: {}'.format(self.state['player1Bets']))
@@ -58,7 +58,7 @@ class Poker:
 
     def refreshState(self):
         self.state['currentPlayer'] = 'Player1'
-        self.state['player1Actions'] = []
+        self.state['player1legalActions'] = []
 
     def successor(self, state, action):
         '''
@@ -67,13 +67,17 @@ class Poker:
         current_player = self.state['currentPlayer']
         if current_player == 'Player1':
             # Player1's first action in a round
-            if len(self.state['player1Actions']) == 0:
+            if len(self.state['player1legalActions']) == 0:
                 if action == 'check':
                     # do nothing
                     pass
                 elif action == 'bet':
                     self.state['player1Bets'].append(1)
                     self.state['player1Stack'] -= 1
+                elif action == 'fold':
+                    # self.state['player1Bets'].append(1)
+                    # self.state['player1Stack'] -= 1
+                    self.state['player1Fold'] = True
                 else:
                     print("Invalid action.")
                     raise
@@ -89,7 +93,7 @@ class Poker:
                     raise
                 self.state['currentPlayer'] = 'Dealer'
             # Append player1's action to the state.
-            self.state['player1Actions'].append(action)
+            self.state['player1legalActions'].append(action)
         elif current_player == 'Player2':
             # if player2 not raise, next player will be Dealer.
             self.state['currentPlayer'] = 'Dealer'
@@ -103,7 +107,7 @@ class Poker:
                 self.state['player2Stack'] -= 1
             elif action == 'raise':
                 raise_amount = 1
-                if self.state['player1Actions'][0] == 'bet':
+                if self.state['player1legalActions'][0] == 'bet':
                     raise_amount = 2
                 self.state['player2Bets'].append(raise_amount)
                 self.state['player2Stack'] -= raise_amount
@@ -130,7 +134,7 @@ class Poker:
         else:
             return False
 
-    def actions(self):
+    def legalActions(self):
         def maxBet(state):
             return min(state['player1Stack'], state['player2Stack'])
 
@@ -210,12 +214,12 @@ action_aggression_map['bet'] = 7
 action_aggression_map['raise'] = 10
 
 def random_player(game):
-    return random.choice(leducGame.actions())
+    return random.choice(leducGame.legalActions())
 
 def aggressive_player(game):
     max_aggression = -10
     chosen_action = None
-    for action in game.actions():
+    for action in game.legalActions():
         if action_aggression_map[action] > max_aggression:
             max_aggression = action_aggression_map[action]
             chosen_action = action
@@ -234,7 +238,7 @@ def baseline_player(game):
             action_preference = ['check', 'fold']
 
         for action in action_preference:
-            if action in game.actions():
+            if action in game.legalActions():
                 return action
 
     if game.player() == 'Player1':
@@ -246,6 +250,42 @@ def baseline_player(game):
         hand_strength = game.evaluator.evaluate(game.state['player2Hand'], game.state['communityCards'])
         hand_strength_pct = game.evaluator.get_five_card_rank_percentage(hand_strength)
         return action_given_hand_strength_pct(hand_strength_pct)
+
+    else:
+        return 'draw_card'
+
+def oracle_player(game, future_cards):
+    def action_given_hand_both_strength_pct(my_hand_strength_pct, opponent_hand_strength_pct, full_community_cards):
+        # print('my_hand_strength_pct: {}, opponent_hand_strength_pct: {}'.format(my_hand_strength_pct, opponent_hand_strength_pct))
+        # fold, check, bet, call, raise
+        if my_hand_strength_pct < opponent_hand_strength_pct:
+                return 'fold'
+        else:
+            if 'raise' in game.legalActions():
+                return 'raise'
+            elif 'call' in game.legalActions():
+                return 'call'
+            elif 'bet' in game.legalActions():
+                return 'bet'
+            else:
+                return 'check'
+
+    full_community_cards = game.state['communityCards']
+    if len(game.state['communityCards']) == 3:
+        full_community_cards = game.state['communityCards'] + future_cards
+    elif len(game.state['communityCards']) == 4:
+        full_community_cards = game.state['communityCards'] + [future_cards[0]]
+
+    hand_strength1 = game.evaluator.evaluate(game.state['player1Hand'], full_community_cards)
+    hand_strength_pct1 = game.evaluator.get_five_card_rank_percentage(hand_strength1)
+    hand_strength2 = game.evaluator.evaluate(game.state['player2Hand'], full_community_cards)
+    hand_strength_pct2 = game.evaluator.get_five_card_rank_percentage(hand_strength2)
+
+    if game.player() == 'Player1':
+        return action_given_hand_both_strength_pct(hand_strength_pct1, hand_strength_pct2, full_community_cards)
+
+    elif game.player() == 'Player2':
+        return action_given_hand_both_strength_pct(hand_strength_pct2, hand_strength_pct1, full_community_cards)
 
     else:
         return 'draw_card'
@@ -266,7 +306,7 @@ print('Random vs. Random')
 num_rounds = 5
 hand_size = 2
 
-utilities = []
+utilities1 = []
 
 # Simulate basline player against random player
 for i in xrange(10000):
@@ -274,10 +314,10 @@ for i in xrange(10000):
     while True:
         if leducGame.isEnd():
             #print('utility: {}'.format(leducGame.utility()))
-            utilities.append(leducGame.utility())
+            utilities1.append(leducGame.utility())
             break
         #print('player: {}'.format(leducGame.player()))
-        #print('legal actions: {}'.format(leducGame.actions()))
+        #print('legal legalActions: {}'.format(leducGame.legalActions()))
         if leducGame.player() == 'Player1':
             action = random_player(leducGame)
         elif leducGame.player() == 'Player2':
@@ -286,34 +326,40 @@ for i in xrange(10000):
             action = 'draw_card'
         #print('action take: {}'.format(action))
         leducGame.successor(leducGame.state, action)
+    if i % 1000 == 0:
+        print("************game:{}***************".format(i))
 
-print('avg utility: {}'.format(sum(utilities)*1.0/len(utilities)))
-# print('utilities: {}'.format(utilities))
+print('avg utility: {}'.format(sum(utilities1)*1.0/len(utilities1)))
+# print('utilities1: {}'.format(utilities1))
 
 print('Baseline vs. Random')
-num_rounds = 5
-hand_size = 2
 
-utilities = []
-
-# Simulate basline player against random player
+utilities2 = []
+# Simulate oracle player against random player
 for i in xrange(10000):
     leducGame = Poker(hand_size=hand_size, num_rounds=num_rounds)
+    future_cards = leducGame.state['deck'].draw(2)
+    leducGame.state['deck'].putBack(future_cards)
     while True:
         if leducGame.isEnd():
             #print('utility: {}'.format(leducGame.utility()))
-            utilities.append(leducGame.utility())
+            utilities2.append(leducGame.utility())
             break
         #print('player: {}'.format(leducGame.player()))
-        #print('legal actions: {}'.format(leducGame.actions()))
+        #print('legal legalActions: {}'.format(leducGame.legalActions()))
         if leducGame.player() == 'Player1':
-            action = baseline_player(leducGame)
+            action = oracle_player(leducGame, future_cards)
         elif leducGame.player() == 'Player2':
             action = random_player(leducGame)
         else:
             action = 'draw_card'
-        #print('action take: {}'.format(action))
+        
+        # print('action take: {}'.format(action))
         leducGame.successor(leducGame.state, action)
+    if i % 1000 == 0:
+        print("************game:{}***************".format(i))
 
-print('avg utility: {}'.format(sum(utilities)*1.0/len(utilities)))
-# print('utilities: {}'.format(utilities))
+print('avg utility: {}'.format(sum(utilities2)*1.0/len(utilities2)))
+# print('utilities2: {}'.format(utilities2))
+
+print('Oracle vs. Random')
